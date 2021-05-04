bf7d972809d4994b5e0c1b5a278e6fc36c187b15
Sparrow48 <abunasib48@gmail.com>
2021-05-04 22:59:04 +0600

    Passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import classes from "./ExpenseItem.module.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 4, 4);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = "1100";
+function ExpenseItem(props) {
   return (
     <div className={classes.ExpenseItem}>
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className={classes.ExpenseItemDescription}>
-        <h2>{expenseTitle}</h2>
-        <div className={classes.ExpenseItemPrice}>{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className={classes.ExpenseItemPrice}>{props.amount}</div>
       </div>
     </div>
   );
