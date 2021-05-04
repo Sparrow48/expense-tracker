77c5d7419ab42731e695cf274aeb34e6acd5ca02
Sparrow48 <abunasib48@gmail.com>
2021-05-04 22:35:27 +0600

    Outputting dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import classes from "./ExpenseItem.module.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 4, 4);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = "1100";
   return (
     <div className={classes.ExpenseItem}>
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className={classes.ExpenseItemDescription}>
-        <h2>Title</h2>
-        <div className={classes.ExpenseItemPrice}>Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className={classes.ExpenseItemPrice}>{expenseAmount}</div>
       </div>
     </div>
   );
