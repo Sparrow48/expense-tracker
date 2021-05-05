80be505b7e82305383df1c4682ed9c0311703d13
Sparrow48 <abunasib48@gmail.com>
2021-05-05 22:05:58 +0600

    working with multiple state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,9 +1,59 @@
+import { useState } from "react";
 import classes from "./ExpenseForm.module.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+
   const titleChangeHandler = event => {
-    console.log(event.target.value);
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    //.... Better approach .....//
+
+    // setUserInput(prevState => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
+
+  const amountChangeHandler = event => {
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+
+    //.... Better approach ....//
+
+    // setUserInput(prevState => {
+    //   return { ...prevState, enteredAmount: event.target.value };
+    // });
+  };
+
+  const dateChangeHandler = event => {
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+
+    //.... Better approach ....//
+
+    // setUserInput(prevState => {
+    //   return { ...prevState, enteredDate: event.target.value };
+    // });
+  };
+
   return (
     <form>
       <div className={classes.NewExpenseControls}>
@@ -13,11 +63,21 @@ const ExpenseForm = () => {
         </div>
         <div className={classes.NewExpenseControl}>
           <label>Amount</label>
-          <input type='number' min='1' siep='1' />
+          <input
+            type='number'
+            onChange={amountChangeHandler}
+            min='1'
+            siep='1'
+          />
         </div>
         <div className={classes.NewExpenseControl}>
           <label>Date</label>
-          <input type='date' min='2020-01-01' max='2025-01-01' />
+          <input
+            type='date'
+            onChange={dateChangeHandler}
+            min='2020-01-01'
+            max='2025-01-01'
+          />
         </div>
       </div>
       <div>
